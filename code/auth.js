9c7eb25a756af1ae1660e375823b94d07064726e
Srushti <135528498+sru36@users.noreply.github.com>
2025-03-24 00:26:44 +0530

    1st year

--- code/auth.js
+++ code/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database');
+const db = require('./database');
 const bcrypt = require('bcrypt');
 
 // Register a new user
